5b2294c5ac5532295991d76f9f424b3ab2977da4
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-10-28 12:41:10 +0200

    Project files

---
+++ Source/main.c
@@ -0,0 +1,100 @@
+//******************************************************************************
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+//******************************************************************************
+
+//******************************************************************************
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
+#include "croutine.h"
+//******************************************************************************
+
+void vLedBlinkBlue(void *pvParameters);
+void vLedBlinkRed(void *pvParameters);
+void vLedBlinkGreen(void *pvParameters);
+void vLedBlinkOrange(void *pvParameters);
+
+#define STACK_SIZE_MIN	1000	/* usStackDepth	- the stack size DEFINED IN WORDS.*/
+
+//******************************************************************************
+int main(void)
+{
+	/*!< At this stage the microcontroller clock setting is already configured,
+	   this is done through SystemInit() function which is called from startup
+	   file (startup_stm32f4xx.s) before to branch to application main.
+	   To reconfigure the default setting of SystemInit() function, refer to
+	   system_stm32f4xx.c file
+	 */
+	
+	/*!< Most systems default to the wanted configuration, with the noticeable 
+		exception of the STM32 driver library. If you are using an STM32 with 
+		the STM32 driver library then ensure all the priority bits are assigned 
+		to be preempt priority bits by calling 
+		NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 ); before the RTOS is started.
+	*/
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+	
+    //LED INIT
+	GPIO_InitTypeDef  GPIO_InitStructure;
+  
+    /* Enable the GPIO_LED Clock */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+    /* Configure the GPIO_LED pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+    //LED INIT END
+	
+	xTaskCreate( vLedBlinkBlue, (const char*)"Led Blink Task Blue", STACK_SIZE_MIN, NULL, tskIDLE_PRIORITY, NULL );
+	xTaskCreate( vLedBlinkRed, (const  char*)"Led Blink Task Red", STACK_SIZE_MIN, NULL, tskIDLE_PRIORITY, NULL );
+	xTaskCreate( vLedBlinkGreen, (const char*)"Led Blink Task Green", STACK_SIZE_MIN, NULL, tskIDLE_PRIORITY, NULL );
+	xTaskCreate( vLedBlinkOrange, (const char*)"Led Blink Task Orange", STACK_SIZE_MIN, NULL, tskIDLE_PRIORITY, NULL );
+	
+	vTaskStartScheduler();
+}
+//******************************************************************************
+
+//******************************************************************************
+void vLedBlinkBlue(void *pvParameters)
+{
+	for(;;)
+	{
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+		vTaskDelay( 500 / portTICK_RATE_MS );
+	}
+}
+
+void vLedBlinkRed(void *pvParameters)
+{
+	for(;;)
+	{
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
+		vTaskDelay( 750 / portTICK_RATE_MS );
+	}
+}
+
+void vLedBlinkGreen(void *pvParameters)
+{
+	for(;;)
+	{
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
+		vTaskDelay( 250 / portTICK_RATE_MS );
+	}
+}
+
+void vLedBlinkOrange(void *pvParameters)
+{
+	for(;;)
+	{
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_15);
+		vTaskDelay( 900 / portTICK_RATE_MS );
+	}
+}
+//******************************************************************************
